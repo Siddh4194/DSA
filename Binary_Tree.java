58e0e22fce2f779290110a0554723c49b067b4eb
Siddhant Dhanaji Kadam <114551539+Siddh4194@users.noreply.github.com>
2023-10-27 21:55:00 +0530

    Update Binary_Tree.java
    min dist between two nodes in bT

--- Binary_Tree.java
+++ Binary_Tree.java
@@ -679,3 +679,42 @@ class Solution {
         return false;
     }
 }
+// find the min distance between the nodes in the binary tree
+class GfG {
+    Node LCA(Node root,int n1, int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+        Node la = LCA(root.left, n1 , n2);
+        Node ra = LCA(root.right, n1 , n2);
+        if(la == null){
+            return ra;
+        } else if(ra == null){
+            return la;
+        }
+        return root;
+    }
+    int distance(Node root, int n){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        int ld = distance(root.left,n);
+        int rd = distance(root.right,n);
+        if(ld == -1 && rd == -1){
+            return -1;
+        } else if(ld == -1){
+            return rd+1;
+        }
+        return ld+1;
+    }
+    int findDist(Node root, int a, int b) {
+        // Your code here
+        Node lca = LCA(root,a,b);
+        int h1 = distance(lca,a);
+        int h2 = distance(lca,b);
+        return h1 + h2;
+    }
+}
