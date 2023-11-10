b08848d0abc6ccfe09a7689bfd96323155fbdf4e
Siddhant Dhanaji Kadam <114551539+Siddh4194@users.noreply.github.com>
2023-11-10 19:13:18 +0530

    Update and rename Strings to Strings.java

--- Strings.java
+++ Strings.java
@@ -172,3 +172,97 @@ class Solution{
         return false;
     }
 }
+
+
+// zigzag pattern for the string
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1){
+          return s;
+        }
+         StringBuilder ans = new StringBuilder();
+         int n = s.length();
+         int main_diff = 2 * (numRows - 1);
+         int diagonalDiff,secondIndex;
+         for(int i = 0; i < numRows ; i++){
+           int index = i;
+           while(index < n){
+             ans.append(s.charAt(index));
+
+            //  for the adjusent element
+             if(i != 0 && i != numRows - 1){
+               diagonalDiff =main_diff - 2 * i;
+               secondIndex = diagonalDiff + index;
+               if(secondIndex < n){
+                 ans.append(s.charAt(secondIndex));
+               }
+             }
+             index +=main_diff;
+           }
+         }
+         return ans.toString();
+    }
+}
+
+// reverse Integere with the limitations
+class Solution {
+    public int reverse(int x) {
+        long reverseNum = 0;
+        
+        while (x != 0) {
+            int reminder = x % 10;
+            x /= 10;
+            
+            reverseNum = reverseNum * 10 + reminder;
+        }
+        if(reverseNum > Integer.MAX_VALUE || reverseNum<Integer.MIN_VALUE){
+            return 0;
+        }
+        if(x<0){
+            return (int)( 0 - reverseNum);
+        }
+        return (int)reverseNum;
+    }
+}
+
+// string to integer
+class Solution {
+    public int myAtoi(String str) {
+        
+        final int len = str.length();
+
+        if(len == 0){
+          return 0;
+        }
+
+        int index = 0;
+
+        while(index < len && str.charAt(index) == ' '){
+          index++;
+        }
+
+        char ch;
+      boolean isNegative = (ch = str.charAt(index)) == '-';
+
+      if(isNegative || str.charAt(index) == '+'){
+        index++;
+      }
+
+      int maxLimit = Integer.MAX_VALUE / 10;
+      int result = 0;
+      while(index < len && isDigit(ch = str.charAt(index))){
+        int digit = ch -'0';
+        if(result > maxLimit || (result == maxLimit && digit > 7)){
+          return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+        result = (result * 10) + digit;
+        index++;
+      }
+      return isNegative ? -result : result;
+    }
+
+    private boolean isDigit(char ch){
+      return ch >= '0' && ch <= '9';
+    }
+    
+}
