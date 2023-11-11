ce0d859746548463b5ce9632896de3e42a8ee054
Siddhant Dhanaji Kadam <114551539+Siddh4194@users.noreply.github.com>
2023-11-11 18:02:48 +0530

    Update linked_list.java

--- linked_list.java
+++ linked_list.java
@@ -298,3 +298,38 @@ class Clone {
         return nodeMap.get(head);
     }
 }
+
+
+// swap the nodes
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+      helper(head);
+        return head;
+    }
+    public void helper(ListNode head){
+      if(head == null || head.next == null){
+        return;
+      }
+      // if(head.next.next != null){
+      //   ListNode temp = head.next.next;
+      //   head.next.next = head;
+      //   head.next = temp;
+      //   helper(temp);
+      // } else {
+        int temp = head.next.val;
+        head.next.val = head.val;
+        head.val = temp;
+        helper(head.next.next);
+      // }
+    }
+}
