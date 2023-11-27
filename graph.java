40b4725a562a93fab3a8e360184c339de1608156
Siddhant Dhanaji Kadam <114551539+Siddh4194@users.noreply.github.com>
2023-11-27 22:48:51 +0530

    Update graph.java

--- graph.java
+++ graph.java
@@ -270,3 +270,44 @@ class Solution {
         }
     }
 }
+// using proper graph above que is solved
+
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) {
+            return -1;
+        }
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] connection : connections) {
+            graph.get(connection[0]).add(connection[1]);
+            graph.get(connection[1]).add(connection[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, graph, visited);
+                components++;
+            }
+        }
+
+        return components - 1;
+    }
+
+    private void dfs(int node, List<List<Integer>> graph, boolean[] visited) {
+        visited[node] = true;
+        for (int neighbor : graph.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, graph, visited);
+            }
+        }
+    }
+}
