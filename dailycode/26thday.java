eeb4f9f44b211f63dcdf93ce82a425e0980c6c51
Siddhant Dhanaji Kadam <114551539+Siddh4194@users.noreply.github.com>
2024-01-07 12:44:12 +0530

    Update 26thday.java

--- dailycode/26thday.java
+++ dailycode/26thday.java
@@ -33,3 +33,22 @@ class Solution {
         return lists;
     }
 }
+// find the largest water area
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0,right = height.length - 1;
+        while(left < right){
+          int currentArea = (right - left) * (height[left] < height[right] ? height[left] : height[right]);
+          if(currentArea > maxArea){
+            maxArea = currentArea;
+          }
+          if(height[left] > height[right]){
+            right--;
+          } else{
+            left++;
+          }
+        }
+        return maxArea;
+    }
+}
